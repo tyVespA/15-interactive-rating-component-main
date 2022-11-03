897916db562a5c27364e66771cfb699152b7a352
tyVespA <migliavacca.marco08@gmail.com>
2022-11-03 12:14:30 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# interactive-rating-component-main
+ Small project to practice handling user interactions and updating the DOM.
---
+++ script.js
@@ -0,0 +1,39 @@
+const evaluation = document.querySelectorAll(".container--rating--rating");
+const submit = document.querySelector(".container--button-submit");
+const containerTwo = document.querySelector(".container-two")
+
+const ratingValue = document.querySelector(".rating-value")
+const one = document.querySelector(".one");
+const two = document.querySelector(".two");
+const three = document.querySelector(".three");
+const four = document.querySelector(".four");
+const five = document.querySelector(".five");
+
+let rating;
+
+evaluation.forEach((button) => {
+   button.addEventListener("click", () => {
+      button.classList.toggle("container--rating--rating")
+      button.classList.toggle("active")
+   })
+})
+
+submit.addEventListener("click", () => {
+   containerTwo.classList.toggle("container-two-display")
+})
+
+one.addEventListener("click", ()=> {
+   ratingValue.textContent = "1";
+})
+two.addEventListener("click", ()=> {
+   ratingValue.textContent = "2";
+})
+three.addEventListener("click", ()=> {
+   ratingValue.textContent = "3";
+})
+four.addEventListener("click", ()=> {
+   ratingValue.textContent = "4";
+})
+five.addEventListener("click", ()=> {
+   ratingValue.textContent = "5";
+})
